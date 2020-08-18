17c6eceb9fa41f71361529f24c56d25987ce3926
David Shestopal <dshestopal@yahoo.com>
2020-08-18 11:43:14 -0700

    pages and components made

---
+++ david-s-dream-store/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Product from './components/Product';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <ProductList />
+      <Details />
+      <Cart />
+      <Default />
+    </React.Fragment>
+  );
+}
+
+export default App;

1dddaf9c406218a065748aa555f6061bdb6b50e5
David Shestopal <dshestopal@yahoo.com>
2020-08-18 12:22:15 -0700

    styled components added

--- david-s-dream-store/src/App.js
+++ david-s-dream-store/src/App.js
@@ -6,7 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navbar from './components/Navbar';
 import ProductList from './components/ProductList';
-
 import Details from './components/Details';
 import Cart from './components/Cart';
 import Default from './components/Default';

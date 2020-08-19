7439a2629d748dd7f3ca3197ce70b301ce40a2bf
David Shestopal <dshestopal@yahoo.com>
2020-08-19 13:15:04 -0700

    products are fetched by id

--- david-s-dream-store/src/components/Button.js
+++ david-s-dream-store/src/components/Button.js
@@ -19,3 +19,23 @@ export const ButtonContainer = styled.button`
     outline: none;
   }
 `;
+
+export const ButtonContainerTwo = styled.button`
+  text-transform: capitalize;
+  font-size: 1.4rem;
+  background: transparent;
+  border: 0.05rem solid var(--mainYellow);
+  color: var(--mainYellow);
+  border-radius: 0.5rem;
+  padding: 0.2rem 0.5rem;
+  cursor: pointer;
+  margin: 0.2rem 0.5rem 0.2rem 0;
+  transition: all 0.5s ease-in-out;
+  &:hover {
+    background: var(--mainYellow);
+    color: var(--mainBlue);
+  }
+  &:focus {
+    outline: none;
+  }
+`;
